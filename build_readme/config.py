e2a598a1beadc14037fe8f1485f4227fe4e29e00
v1siuol <lvzh.louis2013@gmail.com>
2023-09-08 02:19:06 -0400

    Update profile and card styling

--- build_readme/config.py
+++ build_readme/config.py
@@ -7,15 +7,15 @@ class Config:  # pylint: disable=too-few-public-methods
 
     ENABLE_HEADER = True
 
-    SELF_INTRO = 'Grad Student @CMU | ex SWE @FactSet'
+    SELF_INTRO = 'Grad Student @CMU | SWE Intern @eBay | ex SWE @FactSet'
     ENABLE_RECENT_POSTS = True
     BLOG_FEED_URL = 'https://v1siuol.com/rss2.xml'
     POSTS_DISPLAYED_NUM = 5
 
     ENABLE_GITHUB_STATS_CARD = True
-    GITHUB_STATS_CARD = 'https://github-readme-stats.vercel.app/api?username=v1siuol&count_private=true&show_icons=true&hide_title=true&include_all_commits=true'  # pylint: disable=line-too-long
+    GITHUB_STATS_CARD = 'https://github-readme-stats.vercel.app/api?username=v1siuol&count_private=true&show_icons=true&hide_title=true&include_all_commits=true&theme=transparent'  # pylint: disable=line-too-long
 
     ENABLE_WAKATIME_STATS_CARD = True
-    WAKATIME_STATS_CARD = 'https://github-readme-stats.vercel.app/api/wakatime?username=v1siuol&hide_title=true'  # pylint: disable=line-too-long
+    WAKATIME_STATS_CARD = 'https://github-readme-stats.vercel.app/api/wakatime?username=v1siuol&hide_title=true&langs_count=10&layout=compact'  # pylint: disable=line-too-long
 
     ENABLE_FOOTER = True
